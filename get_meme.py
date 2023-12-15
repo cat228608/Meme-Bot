ada02d69e30f198b9226fc0fbe6a8ebfe01210cd
User404 <111229341+cat228608@users.noreply.github.com>
2023-12-15 12:30:18 +0300

    -1 meme database
    Убрал устаревшую базу с мемами

--- get_meme.py
+++ get_meme.py
@@ -5,7 +5,7 @@ import random
 
 async def meme():
     try:
-        random_meme = random.randint(1, 4)
+        random_meme = random.randint(1, 3)
         if random_meme == 1:
             random_site = random.randint(1, 2857)
             url = f"https://www.memify.ru/memes/{random_site}"
@@ -21,11 +21,12 @@ async def meme():
             url = 'https://mem-baza.ru'
             random_site = random.randint(1, 117)
             async with aiohttp.ClientSession() as session:
-                async with session.get(url + f"?page{random_site}") as response:
+                async with session.get(url + f"/page/{random_site}/") as response:
                     content = await response.text()
                     soup = BeautifulSoup(content, "html.parser")
                     items = soup.find_all('img', style='padding:0;border:0;')
                     random_item = random.choice(items)
+                    print(random_item['src'])
                     return f"https://mem-baza.ru{random_item['src']}"
         elif random_meme == 3:
             random_site = random.randint(1, 22)
@@ -33,19 +34,11 @@ async def meme():
                 async with session.get(f"https://www.funomania.ru/mems/page/{random_site}/") as response:
                     content = await response.text()
                     soup = BeautifulSoup(content, "html.parser")
-                    items = soup.find_all("article", {"class": "block story shortstory"})
+                    items = soup.find_all("article", {"class": "jo9"})
                     random_item = random.choice(items)
-                    link = random_item.find("img", {"class": "fr-dib"})["src"]
+                    link = random_item.find("img")["src"]
+                    print(link)
                     return f"https://www.funomania.ru{link}"
-        elif random_meme == 4:
-            async with aiohttp.ClientSession() as session:
-                async with session.get('https://artifact.ru/memes/random/') as response:
-                    html = await response.text()
-                    soup = BeautifulSoup(html, 'html.parser')
-                    dev_elem = soup.find('div', {'class': 'b-post meme-image-block'})
-                    img_elem = dev_elem.find('img')
-                    meme_image_src = img_elem['src']
-                    return f'https://artifact.ru{meme_image_src}'
     except Exception as e:
-        print(e)
-        return 'error'
+    	news = await meme()
+    	return news
